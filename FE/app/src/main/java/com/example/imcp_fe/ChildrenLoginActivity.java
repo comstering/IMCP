dc3b27f253481ace3a0340b3b12fd31a219dae32
BakJinSu <201601824@smail.kongju.ac.kr>
2020-05-24 20:37:32 +0900

    Edit Children Login

---
+++ FE/app/src/main/java/com/example/imcp_fe/ChildrenLoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.imcp_fe;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import javax.xml.datatype.Duration;
+
+public class ChildrenLoginActivity extends AppCompatActivity
+{
+    EditText et_children_new_pw,et_children_re_pw;
+    String sNewPW, sRePW;
+    Button btn_children_create_pw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.children_first_password_page);
+
+        // 비밀번호 입력 초기화
+        et_children_new_pw= (EditText)findViewById(R.id.et_children_new_pw);
+        et_children_re_pw=(EditText)findViewById(R.id.et_children_re_pw);
+        btn_children_create_pw=(Button)findViewById(R.id.btn_children_create_pw);
+
+        sNewPW = et_children_new_pw.getText().toString();
+        sRePW = et_children_re_pw.getText().toString();
+    }
+
+    // 비밀번호 생성 버튼 명령
+    public void btn_children_create_pw(View view)
+    {
+        sNewPW = et_children_new_pw.getText().toString();
+        sRePW = et_children_re_pw.getText().toString();
+
+        if(sNewPW.equals(sRePW))
+        {
+            // 비밀번호 일치
+            Toast.makeText(getApplicationContext(),"비밀번호 맞음", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            // 비밀번호 불일치
+            Toast.makeText(getApplicationContext(),"비밀번호 틀림", Toast.LENGTH_LONG).show();
+        }
+    }
+}
+
+
--- FE/app/src/main/java/com/example/imcp_fe/MainActivity.java
+++ FE/app/src/main/java/com/example/imcp_fe/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.imcp_fe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 아이 버튼 클릭 시 아이 비밀번호 입력창으로 이동.
+        ImageButton ibChildren = (ImageButton)findViewById(R.id.ib_Children);
+        ibChildren.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),ChildrenLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
