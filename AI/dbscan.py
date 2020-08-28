b8e4936671e5ce469311674c6222c131fa22a638
kmg733 <mang906@naver.com>
2020-08-29 05:50:25 +0900

    Make DBSCAN Method And Make Out Code

--- AI/dbscan.py
+++ AI/dbscan.py
@@ -1,6 +1,7 @@
 from scipy.spatial.distance import pdist, squareform
 from sklearn.cluster import DBSCAN
 from haversine import haversine
+from dbscanMethod import doDbscan
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,23 +13,25 @@ df2 = pd.read_csv('./GpsData/CHILD_GPS(한수2).csv')
 df3 = pd.read_csv('./GpsData/CHILD_GPS(김규1).csv')
 df4 = pd.read_csv('./GpsData/CHILD_GPS(김규2).csv')
 '''
-df = pd.read_csv('./GpsData/CHILD_GPS(한수1).csv')
+df = pd.read_csv('./GpsData/CHILD_GPS(김규1).csv')
 
 #Give Columns  Name
-df.columns = ['ChildNum', 'Date', 'latitude', 'longitude']
+df.columns = ['ChildKey', 'Time', 'latitude', 'longitude']
 X = df[['latitude', 'longitude']]
 
-#DataFrame Convert To List
-#geo = df3[['latitude', 'longitude']].values.tolist()
 
 distance_matrix = squareform(pdist(X, (lambda u, v: haversine(u, v))))
 
 db = DBSCAN(eps=0.5, min_samples=3, metric='precomputed')
 y_db = db.fit_predict(distance_matrix)
+
+#Cluster's Info - '-1' is Noise Cluster in X['Cluster'] data
 X['cluster'] = y_db
 
-#Cluster's Info
-print(X['cluster'].drop_duplicates())
+#Drop All of Noise Data
+for i, row in X.iterrows():
+    if row['cluster'] == -1:
+        X.drop(i, inplace=True)
 
 plt.scatter(X['longitude'], X['latitude'], c=X['cluster'])
 plt.show()
